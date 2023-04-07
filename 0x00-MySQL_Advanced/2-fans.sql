9cb52b77a2043f505c0d629ce54d3721de30b1dc
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-07 02:17:49 +0000

    Add solution to task 3

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- An SQL script that ranks country origins of bands, ordered by 
+-- A SQL script that ranks country origins of bands, ordered by 
 -- the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- 
+-- Requirements:
+--
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: band_name and lifespan (in years)
+-- You should use attributes formed and split for computing the lifespan
+-- Your script can be executed on any database
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
