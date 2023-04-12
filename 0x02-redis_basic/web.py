1c2bb9085278072791b70d630f0c278339473480
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-12 23:36:12 +0000

    Update solution fot task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,39 +15,61 @@ Tip: Use http://slowwly.robertomurray.co.uk to simulate
 a slow response and test your caching."""
 
 
+# import redis
+# import requests
+# from functools import wraps
+
+# r = redis.Redis()
+
+
+# def url_access_count(method):
+#     """decorator for get_page function"""
+#     @wraps(method)
+#     def wrapper(url):
+#         """wrapper function"""
+#         key = "cached:" + url
+#         cached_value = r.get(key)
+#         if cached_value:
+#             return cached_value.decode("utf-8")
+
+#             # Get new content and update cache
+#         key_count = "count:" + url
+#         html_content = method(url)
+
+#         r.incr(key_count)
+#         r.set(key, html_content, ex=10)
+#         r.expire(key, 10)
+#         return html_content
+#     return wrapper
+
+
+# @url_access_count
+# def get_page(url: str) -> str:
+#     """obtain the HTML content of a particular"""
+#     results = requests.get(url)
+#     return results.text
+
+
+# if __name__ == "__main__":
+#     get_page('http://slowwly.robertomurray.co.uk')
+
 import redis
 import requests
-from functools import wraps
 
-r = redis.Redis()
+rc = redis.Redis()
 
-
-def url_access_count(method):
-    """decorator for get_page function"""
-    @wraps(method)
-    def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
-
-		# Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        return html_content
-    return wrapper
-
-
-@url_access_count
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    cached_key = f"cached:{url}"
+    count_key = f"count:{url}"
+    
+    cached_value = rc.get(cached_key)
+    if cached_value:
+        return cached_value.decode('utf-8')
+    
+    html_content = requests.get(url).text
+    
+    rc.incr(count_key)
+    rc.set(cached_key, html_content)
+    rc.expire(cached_key, 10)
+    
+    return html_content
